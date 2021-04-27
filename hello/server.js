60539e85517f96eea7b836beff4819adee0af6c1
Angel B. Fernandez <25628695+Avixph@users.noreply.github.com>
2021-04-26 20:00:58 -0400

    third commit

--- hello/server.js
+++ hello/server.js
@@ -2,19 +2,35 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hey there everyone!");
+  res.send("Hey there everyone! Welcome to my webpage 👋🏼");
 });
 
 app.get("/login", (req, res) => {
   res.send("Logged In!");
 });
 
+app.get("/favorite-food", (req, res) => {
+  res.send("Pho");
+});
+
+app.get("/favorite-movie", (req, res) => {
+  res.send("- NULL -");
+});
+
+app.get("/about-me", (req, res) => {
+  res.send(
+    "A detailed-oriented individual with great interpersonal skills, and can collaborate well with peers. With a high interest in designing and coding. Currently looking to pursue a User Experience Design, Front-End Development, or Full-Stack Development role. The goal, when placed on a team will be to continue fostering and growing my knowledge. It’s evident that moving up in roles contains challenges along with interesting projects, willing to go above and beyond for the team and company."
+  );
+});
+
+app.get("/contact", (req, res) => {
+  res.send("Preferred Contact: Email & LinkedIn");
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Express seerver listening on port ${PORT}`);
 });
-
-
 
 /* HTTP request is sent to the http://localhost:3000/
 call the handler function */
